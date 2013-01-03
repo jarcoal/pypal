a60c1d11d99b27959fcfe9f753c5bf777b801c1e
Jared <jarcoal@gmail.com>
2013-01-02 22:33:33 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+	name='pypal',
+	version='0.1',
+	description='PayPal API bindings in Python',
+	author='Jared Morse',
+	author_email='jarcoal@gmail.com',
+	url="https://github.com/jarcoal/pypal",
+	packages=['pypal'],
+	install_requires=['requests >= 1.0.0'],
+    license="BSD",
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Web Environment",
+    ],
+)
