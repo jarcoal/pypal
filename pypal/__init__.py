9a07a031e5db393face7fdce7990c1de995ca341
Jared Morse <jarcoal@gmail.com>
2013-01-04 13:07:19 -0800

    import resources into __init__

--- pypal/__init__.py
+++ pypal/__init__.py
@@ -4,3 +4,8 @@
 
 API_BASE_URL = 'https://svcs.paypal.com'
 API_BASE_URL_DEBUG = 'https://svcs.sandbox.paypal.com'
+
+from adaptive_accounts.resources import AdaptiveAccountsResource
+from adaptive_payments.resources import AdaptivePaymentsResource
+from invoice.resources import InvoiceResource
+from permissions.resources import PermissionsResource
