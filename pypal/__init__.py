bc9bbaec9c912714f08c58d3ce96f9e9a4baff4a
Jared Morse <jarcoal@gmail.com>
2022-09-12 11:23:46 -0700

    Fix import

--- pypal/__init__.py
+++ pypal/__init__.py
@@ -6,4 +6,4 @@ API_BASE_URL = 'https://svcs.paypal.com'
 API_BASE_URL_SANDBOX = 'https://svcs.sandbox.paypal.com'
 
 
-from resources import AdaptiveAccountsResource, AdaptivePaymentsResource, InvoiceResource, PermissionsResource
+from .resources import AdaptiveAccountsResource, AdaptivePaymentsResource, InvoiceResource, PermissionsResource
